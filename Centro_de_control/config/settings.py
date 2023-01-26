4945996e595fd6c33bfa9df5b163467142ce2432
Cesar Guillermo Vazquez Alvarez <cdgva@outlook.com>
2023-01-25 22:04:18 -0600

    Allowed localhost

--- Centro_de_control/config/settings.py
+++ Centro_de_control/config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-0s^z006(3o4%57#@zk27x9o#i)uvd4ab6rc1v%kd2vvdcc%%po
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["10.22.200.20", "0.0.0.0", "127.0.0.1", "localhost"]
 
 
 # Application definition
