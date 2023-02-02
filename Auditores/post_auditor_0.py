6b27c9d9f5590fe4120f8c1ca9fd57f80adf0854
dpaasche <79104094+dpaasche@users.noreply.github.com>
2023-02-02 17:50:56 -0600

    Mac

--- Auditores/post_auditor_0.py
+++ Auditores/post_auditor_0.py
@@ -1,12 +1,15 @@
-import socket
+#import socket
+from getmac import get_mac_address as gma
 import time
 import pandas as pd
 import requests
 import json
 
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+'''s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.connect(("8.8.8.8", 8000))
-IPAddr = s.getsockname()[0]
+IPAddr = s.getsockname()[0]'''
+
+mac = gma()
 
 url = "https://0.0.0.0:8000/api/v1/entries/"
 
@@ -18,7 +21,7 @@ auth = ('admin@cocoa.com', '1234')
 
 df0 = pd.read_csv("../Archivos_trazas/auditor0.csv")
 for i in range(10):
-    df0.loc[i, 'ip_emisor'] = IPAddr
+    df0.loc[i, 'mac_emisor'] = mac
     payload = df0.iloc[i].to_json()
     jpayload = json.loads(payload)
     response = requests.post(url, json=jpayload, headers=headers, auth=auth, verify=False)
