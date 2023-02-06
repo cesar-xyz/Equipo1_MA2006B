c90b74e96603f7c5bfeed57307eda73f2605b7ab
Cesar Guillermo Vazquez Alvarez <cdgva@outlook.com>
2023-02-06 16:25:58 -0600

    Comments for Auditor model

--- main/auditors/models.py
+++ main/auditors/models.py
@@ -1,14 +1,28 @@
+# Importar las clases necesarias de los módulos de Django
 from django.db import models
 from django.utils.translation import gettext as _
+# Importar la biblioteca Paranoid
 from paranoid_model.models import Paranoid
 
 
+# Crear una clase "Auditor" que hereda de la clase "Paranoid"
 class Auditor(Paranoid):
+    # Definir el campo "name" como un campo de tipo CharField
+    # con un tamaño máximo de 128 caracteres y una etiqueta de "name"
     name = models.CharField(_("name"), max_length=128)
+
+    # Definir el campo "mac_address" como un campo de tipo CharField
+    # con un tamaño máximo de 256 caracteres y una etiqueta de "mac address"
     mac_address = models.CharField(_("mac address"), max_length=256)
+
+    # Definir la clase Meta para proporcionar información adicional sobre el modelo
     class Meta:
+        # Establecer el nombre verbal para una sola instancia del modelo como "auditor"
         verbose_name = _("auditor")
+        # Establecer el nombre verbal para varias instancias del modelo como "auditors"
         verbose_name_plural = _("auditors")
 
+    # Definir el método __str__ para devolver una representación en cadena de una instancia del modelo
     def __str__(self):
+        # Devolver "Auditor: [nombre del auditor]"
         return f"Auditor: {self.name}"
--- main/auditors/serializers.py
+++ main/auditors/serializers.py
@@ -1,8 +1,18 @@
+# Este serializador se utiliza para convertir datos de modelos en formato JSON
+# para su transferencia a través de la API.
+
+# Importar el módulo de serializadores de Django Rest Framework
 from rest_framework import serializers
+
+# Importar el modelo "Auditor" desde el módulo de models.py
 from .models import Auditor
 
 
+# Definir la clase "AuditorSerializer" que hereda de "serializers.ModelSerializer"
 class AuditorSerializer(serializers.ModelSerializer):
+    # Definir la clase Meta que contiene información adicional sobre la configuración del serializador
     class Meta:
+        # Especificar el modelo del cual se creará el serializador
         model = Auditor
+        # Especificar una lista de campos que se incluirán en la salida del serializador
         fields = "__all__"
